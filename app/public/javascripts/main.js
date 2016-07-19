39e1535e8140700b463dc1cffe2fe2ffefdd6545
William Yu <william.yu@xingyunzh.com>
2016-07-19 15:16:12 +0800

    side by side web

---
+++ app/public/javascripts/main.js
@@ -0,0 +1,72 @@
+var resultArray = remoteImages.slice();
+var target = resultArray.length - 1;
+var i = 0, j = 1;
+
+function finish() {
+    resultArray.reverse();
+    target = resultArray.length - 1;
+    i = 0, j = 1;
+
+    $("#idBattle").hide();
+    $("#idReport").show();
+
+    var colCount = 0;
+    var row = null;
+    for (var k = 0; k < resultArray.length; k++) {
+        if (colCount == 0) {
+            row = $("<div class = 'row'></div>");
+            $("#idResults").append(row);
+        }
+        var card = $("<div class='col-md-4'><div class='thumbnail result-card'><img src='"+ resultArray[k] +"' alt='pic'> <p>第"+(k+1)+"名</p></div></div>");
+        row.append(card);
+
+        colCount++;
+        if(colCount == 6){
+            colCount = 0;
+        }
+    }
+}
+
+function handleClick(pic) {
+    console.log("i="+i+" j="+j+" target="+target);
+
+    if (pic == 'a') {
+        var temp = resultArray[i];
+        resultArray[i] = resultArray[j];
+        resultArray[j] = temp;
+    }
+    i++;
+    j++;
+
+    if (i == target){
+        target--;
+        if (target == 0){
+            finish();
+            return;
+        }
+        else {
+            i = 0;
+            j = 1;
+        }
+    }
+
+    setImages();
+}
+
+$(function () {
+    setImages();
+
+    $("#idReport").hide();
+
+});
+
+function setImages() {
+    $("#image1").attr("src",resultArray[i]);
+    $("#label1").text("第"+(remoteImages.indexOf(resultArray[i]) + 1) +"号");
+    $("#image2").attr("src", resultArray[j]);
+    $("#label2").text("第"+(remoteImages.indexOf(resultArray[j]) + 1) +"号");
+}
+
+function submit() {
+    
+}
---
+++ app/public/javascripts/remoteImages.js
@@ -0,0 +1,13 @@
+/**
+ * Created by brillwill on 16/7/11.
+ */
+var remoteImages = [
+    "http://img5.imgtn.bdimg.com/it/u=3767192193,1488864582&fm=23&gp=0.jpg",
+    "http://img1.imgtn.bdimg.com/it/u=2537616765,2002461986&fm=23&gp=0.jpg",
+    "http://img4.imgtn.bdimg.com/it/u=577597257,3277350883&fm=23&gp=0.jpg",
+    "http://img0.imgtn.bdimg.com/it/u=1263004785,3713793715&fm=21&gp=0.jpg",
+    "http://img0.imgtn.bdimg.com/it/u=3778841616,799319926&fm=21&gp=0.jpg",
+    "http://img5.imgtn.bdimg.com/it/u=2130300601,3199886642&fm=21&gp=0.jpg",
+    "http://img3.imgtn.bdimg.com/it/u=1691749103,3984242486&fm=21&gp=0.jpg",
+    "http://img3.imgtn.bdimg.com/it/u=2055381668,1275897013&fm=21&gp=0.jpg"
+];
